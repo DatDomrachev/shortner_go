4cfaebb36f794e1cdee03d9e4b2666cdb09230a6
Dmitry Domrachev <D.Domrachev@x5.ru>
2021-12-03 20:30:19 +0300

    http

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -7,7 +7,7 @@ import (
 
 type Config struct {
 	Address string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
-	BaseURL string `env:"BASE_URL" envDefault:"localhost:8080"`
+	BaseURL string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 }
 
 func GetConfig() (*Config, error) {
