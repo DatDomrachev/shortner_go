0c0ec435fd9466cc9c91508d2e6a261b30c11938
Dmitry Domrachev <D.Domrachev@x5.ru>
2021-12-03 19:46:09 +0300

    fix tests?

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -6,8 +6,8 @@ import (
 )
 
 type Config struct {
-	Address string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
-	BaseURL string `env:"BASE_URL" envDefault:"localhost:8080"`
+	Address string `env:"SERVER_ADDRESS" envDefault:"http://localhost:8080"`
+	BaseURL string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 }
 
 func GetConfig() (*Config, error) {
